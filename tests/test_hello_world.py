b5f7f4b8d9c556284fbebe568b66e82b69675d22
Oscar Ligthart <oscarligthart@gmail.com>
2022-04-11 22:24:40 +0200

    added test backbone

---
+++ tests/test_hello_world.py
@@ -0,0 +1,15 @@
+#
+# File: tests/test_hello_world.py
+# Auth: Oscar Ligthart
+# Desc: The test file for testing the hello world print
+#
+######################
+
+from application import hello_world
+
+def test_hello_world():
+    """
+    Method to test the hello world function
+    """
+    # todo implement
+    assert True
