b676a6d85508217c09ea79e405d8463cafc65846
Oscar Ligthart <oscarligthart@gmail.com>
2022-04-11 21:39:01 +0200

    added script for running application

---
+++ scripts/main.py
@@ -0,0 +1,18 @@
+#
+# A simple script to run the application
+#
+##############################################
+
+from application import hello_world
+
+def main():
+    """
+    Main function to run this script
+    """
+
+    # call the hello world function in the main script
+    hello_world()
+
+
+if __name__ == "__main__":
+    main()
